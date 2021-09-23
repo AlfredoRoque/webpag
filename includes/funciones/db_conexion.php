2ea39db85c34f319c6679f16619f39ac6c6c1255
AlfredoRoque <46386649+AlfredoRoque@users.noreply.github.com>
2021-09-22 21:23:38 -0500

    version 1.0

--- includes/funciones/db_conexion.php
+++ includes/funciones/db_conexion.php
@@ -1,4 +1,4 @@
-<?php $conn = new mysqli('localhost','root','','gdlwebcamp');
+<?php $conn = new mysqli('us-cdbr-east-04.cleardb.com','b9dbfe4b261282','1e43acdc','heroku_9e42f258aeb47d4');
 if ($conn->connect_error) {
     echo $error -> $conn->connect_error;
 }
